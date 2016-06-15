cedc8d41a5e458bf643984bcb9f0464bdd613531
Bryan Woods <bryanwoods4e@gmail.com>
2016-06-15 17:48:25 -0400

    Learning Elm, Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,25 @@
+module Main exposing (..)
+
+import Html.App
+import Messages exposing (Msg(..))
+import Models exposing (Model, initialModel)
+import View exposing (view)
+import Update exposing (update)
+import Players.Commands exposing(fetchAll)
+
+init : (Model, Cmd Msg)
+init =
+  (initialModel, Cmd.map PlayersMsg fetchAll)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+main : Program Never
+main =
+  Html.App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
---
+++ src/Models.elm
@@ -0,0 +1,12 @@
+module Models exposing (..)
+
+import Players.Models exposing (Player)
+
+type alias Model =
+  { players : List Player
+  }
+
+initialModel : Model
+initialModel =
+  { players = []
+  }
